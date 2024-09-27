3cf4eaee54a17d73e9472c80314d417c344c50e5
Serg <leyvaserg12@gmail.com>
2024-09-26 23:54:54 -0500

    changed the size of the spaceship, and gave it left-right rotation with input

--- spaceship.gd
+++ spaceship.gd
@@ -1,6 +1,6 @@
 extends Node2D
 class_name Spaceship
-
+@export var move_speed = 5.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$SpaceShipSprite/Area2D.body_entered.connect(_on_body_entered)
@@ -14,4 +14,9 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	pass
+	self.rotation -= 0.0
+	
+	if Input.is_action_pressed("ui_left"):
+		self.rotation -= delta*move_speed
+	if Input.is_action_pressed("ui_right"):
+		self.rotation += delta*move_speed
